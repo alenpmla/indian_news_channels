5b67043673f36f39aafccbaada9889331b54ebdd
Alen John Abraham <alenpmla@gmail.com>
2020-01-13 21:06:16 +0530

    Initial commit
    Signed-off-by: Alen John Abraham <alenpmla@gmail.com>

---
+++ android/app/src/main/java/live/news/malayalam/codme/indian_news_channels/MainActivity.java
@@ -0,0 +1,13 @@
+package live.news.malayalam.codme.indian_news_channels;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
